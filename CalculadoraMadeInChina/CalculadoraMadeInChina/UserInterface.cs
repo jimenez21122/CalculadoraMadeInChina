92427cb4cc95bf97f1c67b05488dee9de27723c7
jimenez21122 <71835827+jimenez21122@users.noreply.github.com>
2020-10-19 17:32:21 +0200

    Print Main Menu Funtion

---
+++ CalculadoraMadeInChina/CalculadoraMadeInChina/UserInterface.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraMadeInChina
+{
+    class UserInterface
+    {
+        public static void PrintMainMenu()
+        {
+            System.Console.WriteLine(" Calculadora Made in China");
+            System.Console.WriteLine("......................................");
+            System.Console.WriteLine("1) sunar una serie de números");
+            System.Console.WriteLine("2) restar una serie de números");
+            System.Console.WriteLine("3) multiplicar una serie de números");
+            System.Console.WriteLine("4) Dividir una serie de números");
+            System.Console.WriteLine("5) Realizar la reíz cuadrada de un número");
+            System.Console.WriteLine("6) Inficar si un número es primo");
+            System.Console.WriteLine("7) Realizar el sumatorio de una serie de números");
+            System.Console.WriteLine("0) Salir de la aplicación");
+        }
+    }
+}
